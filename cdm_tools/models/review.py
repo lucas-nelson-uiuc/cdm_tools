4f5d1b60a6e12fb8695b907bc01dc8c867592353
lunelson-deloitte <lunelson@deloitte.com>
2024-07-11 17:08:12 -0500

    build: adopt kwargs (dict) rather than hardcoded personnel

--- cdm_tools/models/review.py
+++ cdm_tools/models/review.py
@@ -1,33 +1,47 @@
 import itertools
 
 
-def cdm_review(
-    preparer: dict = None,
-    senior_reviewer: dict = None,
-    manager_reviewer: dict = None
-) -> callable:
+def cdm_review(**kwargs) -> callable:
+    """
+    Decorator for making a function 'reviewable', allowing staff to approve or
+    disprove procedures performed. Additionally, tracks staff involved in an
+    engagement to be used in memos (if necessary).
+    """
+    
+    if not hasattr(cdm_review, "_staff"):
+        setattr(cdm_review, "_staff", dict())
+    
     def decorator(func: callable) -> callable:
-        all_staff = tuple(
-            staff
-            for staff in (preparer, senior_reviewer, manager_reviewer)
-            if staff is not None
-        )
         assert (
-            all_staff != tuple()
-        ), ">>> [ERROR] No staff provided. Please provide at least two of preparer, senior_reviewer, or manager_reviewer."
+            kwargs != dict()
+        ), ">>> [ERROR] No staff provided. Please provide at least two of personnel."
         assert (
-            len(all_staff) >= 2
-        ), ">>> [ERROR] Not enough staff provided. Please provide at least two of preparer, senior_reviewer, or manager_reviewer."
+            len(kwargs.keys()) >= 2
+        ), ">>> [ERROR] Not enough staff provided. Please provide at least two personnel."
+
+        # reformat staff in dictionary structure
+        staff = {
+            staff.get("name"): {
+                "title": staff.get("title", "Missing Title"),
+                "signoff": staff.get("signoff", False)
+            }
+            for _, staff in kwargs.items()
+        }
+        
+        # udpate staff listing, check if signoffs are missing
+        cdm_review._staff |= staff
         missing_signoffs = list(
             itertools.compress(
-                all_staff, (not person.get("signoff") for person in all_staff)
+                cdm_review._staff, (not person.get("signoff") for person in cdm_review._staff)
             )
         )
+
+        # switch review status based on signoffs
         func._is_reviewed = True
         if missing_signoffs:
-            print(
-                f">>> [WARNING] Missing signoffs from: {', '.join(staff.get('name') for staff in missing_signoffs)}"
-            )
+            # print(
+            #     f">>> [WARNING] Missing signoffs from: {', '.join(staff.get('name') for staff in missing_signoffs)}"
+            # )
             func._is_reviewed = False
         return func
 
