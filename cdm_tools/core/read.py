b2cd70fd21c393a55903447c2f180889830d791d
lunelson-deloitte <lunelson@deloitte.com>
2024-07-15 19:40:00 -0500

    build: support for fixex-width file reading

--- cdm_tools/core/read.py
+++ cdm_tools/core/read.py
@@ -1,5 +1,8 @@
 import re
 import functools
+import itertools
+
+from typing import Iterable
 
 import pyspark
 from pyspark.sql import DataFrame
@@ -19,3 +22,43 @@ def cp_read(
         fs_func(filepath, recursive=recursive),
     )
     return functools.reduce(union_func, map(read_func, files))
+
+
+def cp_read_fwf(
+    filepath: str,
+    column_mapping: Iterable[tuple[str, int]],
+    column_extract: str = "_c0",
+    drop_extract: bool = True
+) -> DataFrame:
+    """
+    Iteratively extract data from `column_extract` using the column names and positions
+    provided in the `column_mapping` sequence.
+
+    Example
+    -------
+    >>> column_mapping = [
+        ("column1", 1),         # creates `column1` as substring 1:4
+        ("column2", 5),         # creates `column2` as substring 5:14
+        ("column3", 15),        # creates `column3` as substring 15:...
+        # ... additional column-index pairs ...
+        ("columnN", 100),       # creates `columnN` as substring 100:200
+        ("", 200)               # column will not be created since there is no next pair
+    ]
+
+    >>> data = cp__read_fwf(filepath="path/to/fwf.txt", column_mapping=column_mapping)
+    >>> # ... good idea to run `clean_df(data)` to remove extra spaces ...
+    """
+    ERROR_MESSAGE_TYPES = "Please revise your column_mapping. Each pair must be of type (str, int). See example for more details."
+    assert all(isinstance(name, str) and isinstance(index, int) for name, index in column_mapping), ERROR_MESSAGE_TYPES
+
+    ERROR_MESSAGE_ORDER = "Please revise your column mapping. The starting index of all pairs must be in ascending order."
+    assert column_mapping == sorted(column_mapping, key=lambda pair: pair[1]), ERROR_MESSAGE_ORDER
+
+    return (
+        cp.read(filepath)
+        .withColumns({
+            column: F.substring(column_extract, pos=start, len=end - start)
+            for (column, start), (_, end) in itertools.pairwise(column_mapping)
+        })
+        .drop(column_extract if drop_extract else "")
+    )
