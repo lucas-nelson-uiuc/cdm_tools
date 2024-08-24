11e543fc3b224234e311a31651fc419c4733bd2f
lunelson-deloitte <lunelson@deloitte.com>
2024-08-24 12:34:33 -0500

    build: update versioning across platforms

--- cdm_tools/models/templates/journal_entry_testing.py
+++ cdm_tools/models/templates/journal_entry_testing.py
@@ -1,33 +1,192 @@
-from ..common import CommonDataModel
+from cdm_tools.models.common import CommonDataModel
 
 import decimal
 import datetime
 
 
-class GeneralLedgerDetail(CommonDataModel):
+from typing import Optional
+import datetime
+import decimal
+
+from pydantic import BaseModel, Field
+
+
+# class CommonDataModel(BaseModel):
+#     class Config:
+#         arbitrary_types_allowed = True
+#         extra = "forbid"
+
+#     @classmethod
+#     def get_fields(cls):
+#         return cls.__annotations__.items()
+
+#     @classmethod
+#     def get_request_form(cls):
+#         return {
+#             field: {"name": field_info.alias or field, "dtype": field_info.annotation}
+#             for field, field_info in cls.model_fields.items()
+#         }
+
+
+class GeneralLedgerDetailModel(CommonDataModel):
+    ### client fields
     entity_id: str
+    entity_name: str
+    chart_of_accounts: str
+
+    ### entry fields
+    account_number: str
+    account_description: Optional[str]
+    journal_number: str
+    journal_header_description: Optional[str]
+    journal_line_number: Optional[str]
+    journal_line_description: Optional[str]
+    is_standard: str
+    location: Optional[str]
+    is_manual: Optional[str]
+    business_area: Optional[str]
+    controlling_area_for_cost_and_profit_center: Optional[str]
+    cost_center: Optional[str]
+    profit_center: Optional[str]
+    project: Optional[str]
+    custom_exclusion: Optional[str]
+    ledger_id: Optional[str]
+    ledger_group: Optional[str]
+
+    ### datetime fields
     fiscal_period: int
+    fiscal_year: int
     date_effective: datetime.date
     date_posted: datetime.date
+    time_posted: datetime.datetime
+    date_updated: Optional[datetime.date]
+    time_updated: Optional[datetime.datetime]
+    date_approved: Optional[datetime.date]
+    time_approved: Optional[datetime.datetime]
+    extract_date: Optional[datetime.date]
+    import_date: Optional[datetime.date]
+
+    ### currency fields
     net_amount_ec: decimal.Decimal
+    debit_amount_ec: Optional[decimal.Decimal]
+    credit_amount_ec: Optional[decimal.Decimal]
+    entity_currency_ec: str = Field(pattern="^\w{2,5}$")
+    net_amount_oc: decimal.Decimal
+    debit_amount_oc: Optional[decimal.Decimal]
+    credit_amount_oc: Optional[decimal.Decimal]
+    original_currency_oc: str = Field(pattern="^\w{2,5}$")
+    net_amount_gc: decimal.Decimal
+    debit_amount_gc: Optional[decimal.Decimal]
+    credit_amount_gc: Optional[decimal.Decimal]
+    group_currency_gc: str = Field(pattern="^\w{2,5}$")
+    dc_indicator: Optional[str]
+    exchange_rate: Optional[float]
+    foreign_exchange_date: Optional[datetime.date]
+    forex_conversion_method: Optional[str]
+
+    ### categorical variables
+    transaction_type: str
+    transaction_type_description: Optional[str]
+    source: str
+    source_description: Optional[str]
+    userid_entered: Optional[str]
+    user_name_entered: Optional[str]
+    userid_approved: Optional[str]
+    user_name_approved: Optional[str]
+    updated_by: Optional[str]
+    name_of_user_updated: Optional[str]
+    misc1: Optional[str]
+    misc2: Optional[str]
+    misc3: Optional[str]
+    misc4: Optional[str]
+    misc5: Optional[str]
+
+    ### chart of accounts fields
+    coa_account_key: Optional[str]
 
 
-class TrialBalance(CommonDataModel):
+class TrialBalanceModel(CommonDataModel):
+    ### client fields
     entity_id: str
+    entity_name: str
+    chart_of_accounts: str
+
+    ### account fields
     account_number: str
-    account_description: str
+    account_description: Optional[str]
+
+    ### datetime fields
+    fiscal_period: int = Field(ge=1, le=12)
+    fiscal_year: int
     period_end_date: datetime.date
-    balance: decimal.Decimal
+    period_type: str = Field(pattern="(BEG|END)")
+    extract_date: Optional[datetime.date]
+    import_date: Optional[datetime.date]
+
+    ### currency fields
+    ending_balance_ec: decimal.Decimal
+    entity_currency_ec: str = Field(pattern="^\w{2,5}$")
+
+    ending_balance_gc: decimal.Decimal
+    group_currency_gc: str = Field(pattern="^\w{2,5}$")
+
+    beginning_balance_ec: Optional[decimal.Decimal]
+    period_activity_ec: Optional[decimal.Decimal]
+    adjusted_ec: Optional[decimal.Decimal]
+    adjusted_journal_entry_ec: Optional[decimal.Decimal]
+    reclassification_journal_entry_ec: Optional[decimal.Decimal]
+    preliminary_ec: Optional[decimal.Decimal]
+    
+    ending_balance_gc: decimal.Decimal
+    beginning_balance_gc: Optional[decimal.Decimal]
+    period_activity_gc: Optional[decimal.Decimal]
+    adjusted_gc: Optional[decimal.Decimal]
+    adjusted_journal_entry_gc: Optional[decimal.Decimal]
+    reclassification_journal_entry_gc: Optional[decimal.Decimal]
+    consolidation_journal_entry_gc: Optional[decimal.Decimal]
+    preliminary_gc: Optional[decimal.Decimal]
+
+    ### categorical variables
+    ledger_id: Optional[str]
+    ledger_group: Optional[str]
+    misc1: Optional[str]
+    misc2: Optional[str]
+
+    ### chart of accounts fields
+    coa_account_key: Optional[str]
 
 
-class ChartOfAccounts(CommonDataModel):
+class ChartOfAccountsModel(CommonDataModel):
+    ### client fields
     entity_id: str
+    entity_name: str
+    chart_of_accounts: str
+
+    ### account fields
     account_number: str
     account_description: str
+
+    ### datetime fields
+    revision_date: Optional[datetime.date]
+    extract_date: Optional[datetime.date]
+    import_date: Optional[datetime.date]
+
+    ### categorical variables
+    content_area_id: Optional[str]
+    content_area: Optional[str]
+    tags: Optional[str]
+
+    ### chart of accounts fields
     financial_statement_line: str
-    financial_statement_line_number: int
-    financial_statement_category: str
-    financial_statement_subtotal_category: str
+    financial_statement_line_num: str
+    financial_statement_category: str = Field(pattern="(Assets|Liabilities|Equity|Revenue|Expenses)")
+    financial_statement_category_id: Optional[str]
+    financial_statement_subtotal_category: str = Field(pattern="(Assets|Liabilities|Equity)")
+    financial_statement_subtotal_category_id: Optional[str]
+    financial_statement_type: str = Field(pattern="(Balance Sheet|Income Statement)")
     account_grouping_1: str
-    account_grouping_1_number: int
+    account_grouping_1_num: str
     abcotd: str
+    abcotd_id: Optional[str]
+    abcotd_significance: Optional[str]
+    coa_account_key: Optional[str]
--- cdm_tools/models/transform.py
+++ cdm_tools/models/transform.py
@@ -1,6 +1,8 @@
+import datetime
+import decimal
 import functools
 
-from pyspark.sql import types, functions as F
+from pyspark.sql import types as T, functions as F
 
 from .types import PYDANTIC_TYPES, DATE_FORMATS, TIMESTAMP_FORMATS
 
@@ -12,59 +14,52 @@ def cdm_transform(model):
             data = func(*args, **kwargs)
             all_transformations = dict()
             for field, field_info in model.model_fields.items():
+                
                 # gather, extract metadata
                 all_transformations[field] = dict()
                 field_name = field_info.alias or field
-                field_type = PYDANTIC_TYPES.get(field_info.annotation, types.NullType())
-
+                field_type = PYDANTIC_TYPES.get(field_info.annotation, T.NullType())
+                
                 if field_name in data.columns:
                     # rename columns if alias provided
                     if field_name != field:
-                        all_transformations[field]["rename"] = (
-                            f"{field_name} to {field}"
-                        )
+                        all_transformations[field]["rename"] = f"{field_name} to {field}"
                         data = data.withColumn(field, F.col(field_name))
 
                     # cast to specified data type; attempt multiple formats for dates, timestamps
-                    if not isinstance(field_type, types.StringType):
+                    if not isinstance(field_type, T.StringType):
                         all_transformations[field]["cast"] = (
                             f"{field} as {field_type.__class__.__name__}"
                         )
-                    if isinstance(field_type, types.DateType):
+                    if isinstance(field_type, T.DateType):
                         if field_info.json_schema_extra:
-                            date_format = field_info.json_schema_extra.get(
-                                "date_format", "MM/dd/yyyy"
-                            )
+                            date_format = field_info.json_schema_extra.get("date_format", "MM/dd/yyyy")
                             formatted_dates = [F.to_date(F.col(field), date_format)]
                         else:
                             formatted_dates = [
                                 F.to_date(F.col(field), fmt) for fmt in DATE_FORMATS
                             ]
                         data = data.withColumn(field, F.coalesce(*formatted_dates))
-                    elif isinstance(field_type, types.TimestampType):
+                    elif isinstance(field_type, T.TimestampType):
                         formatted_timestamps = [
-                            F.to_timestamp(F.col(field), fmt)
-                            for fmt in TIMESTAMP_FORMATS
+                            F.to_timestamp(F.col(field), fmt) for fmt in TIMESTAMP_FORMATS
                         ]
-                        data = data.withColumn(
-                            field,
-                            F.date_format(
-                                F.coalesce(*formatted_timestamps), "MM/dd/yyyy HH:mm:ss"
-                            ),
-                        )
+                        data = data.withColumn(field, F.date_format(F.coalesce(*formatted_timestamps), "MM/dd/yyyy HH:mm:ss"))
                         # data = data.withColumn(field, date_format(F.col(field).cast(field_type), "MM/dd/yyyy HH:mm:ss"))
-                    elif isinstance(field_type, types.DecimalType):
+                    elif isinstance(field_type, T.DecimalType):
                         data = data.withColumn(
                             field,
                             F.regexp_replace(
-                                F.regexp_replace(F.col(field), "\(", "-"),
-                                "[^-\d.eE]+",
-                                "",
-                            ).cast(field_type),
+                                F.regexp_replace(
+                                    F.col(field),
+                                    "\(", "-"
+                                ),
+                                "[^-\d.eE]+", ""
+                            ).cast(field_type)
                         )
                     else:
                         data = data.withColumn(field, F.col(field).cast(field_type))
-
+                
                 # mutate field according to default value, if provided
                 if field_info.default_factory:
                     data = data.withColumn(field, field_info.default_factory("x"))
@@ -75,10 +70,8 @@ def cdm_transform(model):
                 # mutate fields with null values
                 if field_name not in data.columns:
                     all_transformations[field]["mutate"] = "default assigned as NULL"
-                    # assert isinstance(field_type, types.NullType), f"Missing mapping for required field: {field_name}"
-                    data = data.withColumn(
-                        field_name, F.lit(None).cast(types.StringType())
-                    )
+                    # assert isinstance(field_type, T.NullType), f"Missing mapping for required field: {field_name}"
+                    data = data.withColumn(field_name, F.lit(None).cast(T.StringType()))
 
             for field, transformation in all_transformations.items():
                 if transformation:
