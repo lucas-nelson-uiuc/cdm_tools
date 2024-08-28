20a1c32898c4e310c14dd7b82cc9efdcc2e8d147
lunelson-deloitte <lunelson@deloitte.com>
2024-08-28 12:49:53 -0500

    build: update imports for JET

--- cdm_tools/models/templates/journal_entry_testing/__init__.py
+++ cdm_tools/models/templates/journal_entry_testing/__init__.py
@@ -1,11 +1,12 @@
 from general_ledger_detail import GeneralLedgerDetailModel
-from trial_balance import TrialBalanceModel
+from trial_balance import TrialBalanceModel, OmniaTrialBalanceModel
 from chart_of_accounts import ChartOfAccountsModel
 from analytic import JournalEntryTestingAnalytic
 
 __all__ = [
     "GeneralLedgerDetailModel",
     "TrialBalanceModel",
+    "OmniaTrialBalanceModel",
     "ChartOfAccountsModel",
     "JournalEntryTestingAnalytic",
 ]
